8f1419d45b1b537c3c88b212470ebe09c984b40b
tonioluc <antoniorandria3342@gmail.com>
2025-07-08 02:20:17 +0300

    feat : design

--- inc/navigation.php
+++ inc/navigation.php
@@ -2,8 +2,8 @@
     <h2>Menu</h2>
     <ul>
         <li><a href="ajout-fond.php"><i class="fa fa-plus-square"></i> Ajout de fond</a></li>
-        <li><a href="type-pret.php"><i class="fa fa-plus"></i> Creation type de prêt</a></li>
-        <!-- <li><a href="creation_type_pret.php"><i class="fa fa-cogs"></i> Création type de prêt</a></li> -->
+        <li><a href="type-pret.php"><i class="fa fa-list-alt"></i> Creation type de prêt</a></li>
+        <li><a href="tableau-remboursement-pret.php"><i class="fa fa-table"></i> Tableau remboursement prêt</a></li>
         <!-- <li><a href="gestion_pret.php"><i class="fa fa-book"></i> Gestion des prêts</a></li> -->
     </ul>
 </div>
