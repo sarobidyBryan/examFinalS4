4c4cc045c299e1027d8a5fb0ede44d652093d181
sarobidyBryan <raybryansarobidy@gmail.com>
2025-07-08 13:06:53 +0300

    front comparaison

--- comparaison.php
+++ comparaison.php
@@ -1,6 +1,18 @@
-<link rel="stylesheet" href="ws/assets/css/vs-comparaison.css">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <?php include('inc/head.php') ?>
+    <title>Comparaison-VS</title>
+</head>
+<body>
+    <link rel="stylesheet" href="ws/assets/css/vs-comparaison.css">
 <script defer src="ws/assets/js/base.js"></script>
-<div class="comparison-container">
+<?php include('inc/navigation.php') ?>
+<div class="main-content">
+    <div class="btn comparison-container">
+        <h2>Comparaison entre 2 simulations</h2>
     <button onclick="exporterComparaisonPDF()">Exporter en PDF</button>
     <div class="vs-container">
         <div class="vs-icon">VS</div>
@@ -25,3 +37,6 @@
 </div>
 
 <script defer src="ws/assets/js/vs-data.js"></script>
+</div>
+</body>
+</html>
